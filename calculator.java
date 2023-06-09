5bb91482a100ed9c9890de73f0a6103547a57af5
Ashraful Islam <ashraful52038@gmail.com>
2023-06-09 18:05:19 +0600

    First Commit

--- calculator.java
+++ calculator.java
@@ -1,15 +1,49 @@
+import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class calculator implements ActionListener {
 
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functioButtons = new JButton[8];
+    JButton addButton, subButton, mulButtonm, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
     calculator() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 300, 50);
+        textfield.setFont(myFont);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        calculator calc = new calculator();
 
     }
 
-    public static void main(String[] args){
-
-        calculator calc= new calculator()
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 
 }
